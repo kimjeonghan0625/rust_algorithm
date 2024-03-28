89f5ad82c3b4ac57f912cc5ab36335afeeefe4df
jh-ubuntu <mineship@pusan.ac.kr>
2024-03-29 03:05:13 +0900

    Merge Sort 구현 다시 해보기

--- src/rust_algorithm/sort/merge_sort.rs
+++ src/rust_algorithm/sort/merge_sort.rs
@@ -1,43 +1,77 @@
 pub fn merge_sort<T: PartialOrd + Copy>(arr: &mut [T]) {
+    if arr.len() <= 1 { return }
     let mid = arr.len() / 2;
-    if mid == 0 {
-        return;
-    }
-
-    merge_sort(&mut arr[0..mid]);
+    merge_sort(&mut arr[..mid]);
     merge_sort(&mut arr[mid..]);
-
-    let mut ret = arr.to_vec();
-
-    merge(&arr[0..mid], &arr[mid..], &mut ret[..]);
-
-    arr.copy_from_slice(&ret);
+    let mut temp_array = arr.to_vec();
+    merge(&arr[..mid], &arr[mid..], &mut temp_array[..]);
+    arr.copy_from_slice(&temp_array[..])
 }
 
-fn merge<T: PartialOrd + Copy>(a: &[T], b: &[T], ret: &mut [T]) {
-    let mut left = 0;
-    let mut right = 0;
-    let mut index = 0;
-
-    while left < a.len() && right < b.len() {
-        if a[left] <= b[right] {
-            ret[index] = a[left];
-            index += 1;
-            left += 1;
+fn merge<T: PartialOrd + Copy>(a: &[T], b: &[T], temp: &mut [T]) {
+    let mut a_index = 0;
+    let mut b_index = 0;
+    let mut temp_index = 0;
+    while a_index < a.len() && b_index < b.len() {
+        if a[a_index] <= b[b_index] {
+            temp[temp_index] = a[a_index];
+            temp_index += 1;
+            a_index += 1;
         } else {
-            ret[index] = b[right];
-            index += 1;
-            right += 1;
+            temp[temp_index] = b[b_index];
+            temp_index += 1;
+            b_index += 1;
+            
         }
     }
-
-    if left < a.len() {
-        ret[index..].copy_from_slice(&a[left..]);
+    if a_index < a.len() {
+        temp[temp_index..].copy_from_slice(&a[a_index..]);
     }
-    if right < b.len() {
-        ret[index..].copy_from_slice(&b[right..]);
+    if b_index < b.len() {
+        temp[temp_index..].copy_from_slice(&b[b_index..]);
     }
 }
+
+// pub fn merge_sort<T: PartialOrd + Copy>(arr: &mut [T]) {
+//     let mid = arr.len() / 2;
+//     if mid == 0 {
+//         return;
+//     }
+
+//     merge_sort(&mut arr[0..mid]);
+//     merge_sort(&mut arr[mid..]);
+
+//     let mut ret = arr.to_vec();
+
+//     merge(&arr[0..mid], &arr[mid..], &mut ret[..]);
+
+//     arr.copy_from_slice(&ret);
+// }
+
+// fn merge<T: PartialOrd + Copy>(a: &[T], b: &[T], ret: &mut [T]) {
+//     let mut left = 0;
+//     let mut right = 0;
+//     let mut index = 0;
+
+//     while left < a.len() && right < b.len() {
+//         if a[left] <= b[right] {
+//             ret[index] = a[left];
+//             index += 1;
+//             left += 1;
+//         } else {
+//             ret[index] = b[right];
+//             index += 1;
+//             right += 1;
+//         }
+//     }
+
+//     if left < a.len() {
+//         ret[index..].copy_from_slice(&a[left..]);
+//     }
+//     if right < b.len() {
+//         ret[index..].copy_from_slice(&b[right..]);
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
